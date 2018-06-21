a96d4a1b430edfd46acab5a5cbf2885f4c5be5e2
Richard L Zarth III <rlziii@icloud.com>
2018-06-20 21:37:04 -0400

    Initial Commit

---
+++ ScreenBreak/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ScreenBreak
+//
+//  Created by Richard L Zarth III on 2018-06-20.
+//  Copyright © 2018 Richard L Zarth III. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
