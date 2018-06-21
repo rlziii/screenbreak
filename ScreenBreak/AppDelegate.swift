9ae4f597c13265716ad0e59726044aaf4e32b4a5
Richard L Zarth III <rlziii@icloud.com>
2018-06-20 22:24:04 -0400

    Add menu bar icon, remove dock icon, and remove the main window.

--- ScreenBreak/AppDelegate.swift
+++ ScreenBreak/AppDelegate.swift
@@ -2,12 +2,21 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.squareLength)
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        print("Hello, ScreenBreak!")
+        if let button = statusItem.button {
+            button.image = NSImage(named:NSImage.Name("StatusBarIcon"))
+            button.action = #selector(printTitle(_:))
+        }
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
+    }
+    
+    @objc func printTitle(_ sender: Any?) {
+        print("Hello, ScreenBreak!")
     }
 
 }
