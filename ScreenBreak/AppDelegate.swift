8cacc8b1476ba08ca796289611890e135bb40207
Richard L Zarth III <rlziii@icloud.com>
2018-06-20 21:44:09 -0400

    Clean up.

--- ScreenBreak/AppDelegate.swift
+++ ScreenBreak/AppDelegate.swift
@@ -1,26 +1,14 @@
-//
-//  AppDelegate.swift
-//  ScreenBreak
-//
-//  Created by Richard L Zarth III on 2018-06-20.
-//  Copyright © 2018 Richard L Zarth III. All rights reserved.
-//
-
 import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        print("Hello, ScreenBreak!")
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
     }
-
 
 }
 
--- ScreenBreak/ViewController.swift
+++ ScreenBreak/ViewController.swift
@@ -1,27 +1,15 @@
-//
-//  ViewController.swift
-//  ScreenBreak
-//
-//  Created by Richard L Zarth III on 2018-06-20.
-//  Copyright © 2018 Richard L Zarth III. All rights reserved.
-//
-
 import Cocoa
 
 class ViewController: NSViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override var representedObject: Any? {
         didSet {
-        // Update the view, if already loaded.
         }
     }
-
 
 }
 
